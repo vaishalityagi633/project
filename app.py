21e7f35b3bd68418dc3c444ad123185df2c1a0f7
Vaishali Tyagi <151373272+vaishalityagi633@users.noreply.github.com>
2024-04-16 10:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,26 @@
-import os
-from flask import Flask, render_template, request, url_for, redirect,session
-from flask import flash
+from flask import Flask, flash, render_template, request, redirect, url_for, session
+import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
-import pyodbc
-database='PROJECT'
+import psycopg2
+from flask import request
+
 
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET_KEY','fcd6741a4ad8ad430ff636bfd04ae5bed4286b6eeaefdf3edf06d02695782441')
+app.secret_key = '22771d8a39fd4fca1531468593ff6006'
 
 
-
-#conn= pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT')
-#cursor=conn.cursor()
-#cursor.execute("Select * from Records.Book")
-#for row in cursor.fetchall():
-    #print(row)
-def insert_data(database,data):
-             with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
-                cursor=conn.cursor()
-                query=f'''INSERT INTO Record.Book (Title,ISBN,Genre,PublishedDate,QuantityInStock) VALUES (?,?,?,?,?)'''
-                cursor.execute(query,data)
-                conn.commit()
-def insert_datamember(database,data):
-             with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
-                cursor=conn.cursor()
-                query=f'''INSERT INTO Record.Member (MemberName,ContactNumber,Email,Address) VALUES (?,?,?,?)'''
-                cursor.execute(query,data)
-                conn.commit()    
-
-def insert_dataissue(database,data):
-             with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003,1433;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
-             
-                cursor=conn.cursor()
-                query=f'''INSERT INTO Record.Issues(Title,MemberName,IssueDate,ReturnDate) VALUES (?,?,?,?)'''
-                cursor.execute(query,data)
-                conn.commit()                                                
-
-
-
+# PostgreSQL connection parameters
+db_params = {
+    'dbname': 'PROJECT',
+    'user': 'postgres',
+    'password': 'smart@vt2',
+    'host': 'localhost', 
+    'port': 5432, 
+}
+#@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
 @app.route('/')
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -45,9 +28,9 @@ def signin():
         username = request.form['username']
         password = request.form['password']
  
-        conn =  pyodbc.connect('DRIVER={SQL Server};SERVER=host.docker.internal;DATABASE=PROJECT;Trusted_Connection=yes')
+        conn =  psycopg2.connect(**db_params)
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM Record.Staff WHERE Staffname = ?', (username,))
+        cursor.execute('SELECT * FROM Staff WHERE Staffname = %s', (username,))
         user = cursor.fetchone()
         conn.close()
         
@@ -61,10 +44,9 @@ def signin():
         
         return render_template('signin.html')
     return render_template('signin.html')
-#@app.route('/')
-@app.route('/home')
-def home():
-   return render_template('index.html')
+
+
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -77,19 +59,18 @@ def signup():
         #print(hashed_password)
  
         try:
-            conn = pyodbc.connect('DRIVER={SQL Server};SERVER=host.docker.internal;DATABASE=PROJECT;Trusted_Connection=yes')
+            conn = psycopg2.connect(**db_params)
             cursor = conn.cursor()
-            cursor.execute('INSERT INTO Record.Staff (Staffname, Password) VALUES (?,?)', (username, hashed_password))
+            cursor.execute('INSERT INTO Staff(Staffname, Password) VALUES (%s,%s)', (username, hashed_password))
             conn.commit()
             conn.close()
  
             return redirect(url_for('signin'))
  
-        except pyodbc.Error as e:
+        except psycopg2.Error as e:
             return f"An error occurred during signup. Error details: {e}"
  
     return render_template('signup.html')
-
 @app.route('/registermember',methods=['GET','POST'])
 def registermember():
     
@@ -101,23 +82,24 @@ def registermember():
         memail=request.form['emailid']
         maddress=request.form['address']
        # mname_lower=mname.lower()
-        data=(mname,mcontact,memail,maddress)
+        con = psycopg2.connect(**db_params)
+        cursor = con.cursor()
+        cursor.execute('INSERT INTO Member(MemberName,ContactNumber,Email,Address) VALUES (%s, %s, %s,%s)', (mname,mcontact,memail,maddress))
+        con.commit()
         
-
-        insert_datamember(database,data)
         return render_template('registermember.html')
     return render_template('registermember.html')
   
 @app.route('/displaymember')
 def displaymember():
-    with pyodbc.connect('DRIVER={SQL Server};SERVER=host.docker.internal;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
-        cursor=conn.cursor()
-        query=f'select * from Record.Member'
-        cursor.execute(query)
-        data=cursor.fetchall()
-    return render_template('displaymember.html',members=data,info="Registered")
-
-
+      with psycopg2.connect(**db_params) as conn:
+    
+       cursor=conn.cursor()
+      query=f'select * from Member'
+      cursor.execute(query)
+      data=cursor.fetchall()
+      print(data)
+      return render_template('displaymember.html',members=data,info="Registered")
 @app.route('/register',methods=['GET','POST'])
 def register():
     
@@ -135,35 +117,36 @@ def register():
         btitle_lower = btitle.lower()
         data=(btitle_lower,bISBN,genre,pdate,quantity)
         # Check if both the book and member exist
-        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=host.docker.internal;DATABASE=PROJECT;Trusted_Connection=yes')
+        conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
         
         # Check if the book exists
-        cursor.execute('SELECT * FROM Record.Book WHERE LOWER(Title) = ?', (btitle_lower,))
+        cursor.execute('SELECT * FROM Book WHERE LOWER(Title) = %s', (btitle_lower,))
         book = cursor.fetchone()
-       
-        
-        conn.close()
+        #conn.close()
         
         # If  book  exist
         if book :
             
                flash('Book already exist','error') 
         else:
-                # Insert the book record into the database
-          insert_data(database,data)
-          flash('Book registered successfully.', 'success')
+           
+           cursor.execute('INSERT INTO Book(Title,ISBN,Genre,PublishedDate,QuantityInStock) VALUES (%s, %s, %s,%s,%s)', (btitle,bISBN,genre,pdate,quantity))
+     # Insert the book record into the database
+           conn.commit()
+           flash('Book registered successfully.', 'success')
         return render_template('registerbook.html')
     return render_template('registerbook.html')
 
 
 @app.route('/display')
 def display():
-    with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+    with psycopg2.connect(**db_params) as conn:
         cursor=conn.cursor()
-        query=f'select * from Record.Book'
+        query=f'select * from Book'
         cursor.execute(query)
         data=cursor.fetchall()
+        print(data)
     return render_template('display.html',books=data,info="Registered")
 
 @app.route('/searchbook',methods=['POST'])
@@ -173,9 +156,9 @@ def searchbook():
         search_term=search_term.split()
         
         filt_books=[]
-        with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+        with  psycopg2.connect(**db_params) as conn:
             cursor=conn.cursor()
-            query=f'select * from Record.book '
+            query=f'select * from Book '
             cursor.execute(query)
             rows=cursor.fetchall()
         for term in search_term:
@@ -187,11 +170,6 @@ def searchbook():
                         if term.lower() in row_items.lower():
                             filt_books.append(row)
                             break
-                        #print(filt_books)
-         #filt_books=list(set(filt_books))
-                
-                
-        
         return render_template('searchbook.html',books=filt_books,info="Searched")
 
 @app.route('/update/<id>',methods=['GET','POST'])
@@ -201,30 +179,31 @@ def update(id):
         
         bQty=request.form['bookQuantity']
         books=(bQty,id)
-        query=f'''UPDATE Record.Book
+        query=f'''UPDATE Book
                 SET 
-                    QuantityInStock=?
-                WHERE BookID=?'''
-        with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+                    QuantityInStock=%s
+                WHERE BookID=%s'''
+        with  psycopg2.connect(**db_params) as conn:
 
             cursor=conn.cursor()
             cursor.execute(query,books)
             conn.commit()
         return redirect(url_for('display'))
 
-    with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+    with  psycopg2.connect(**db_params)as conn:
         cursor=conn.cursor()
-        query=f'select * from Record.Book where BookID=?'
+        query=f'select * from Book where BookID=%s'
         cursor.execute(query,(id,))
         data=cursor.fetchall()
         update_pr=data[0]
     print(update_pr)
     return render_template('update.html',books=update_pr)
+
 @app.route('/delete/<id>')
 def delete(id):
-    with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+    with  psycopg2.connect(**db_params) as conn:
         cursor=conn.cursor()
-        query=f'DELETE from Record.Book where BookID=?'
+        query=f'DELETE from Book where BookID=%s'
         cursor.execute(query,(id,))
         conn.commit()
     return redirect(url_for('display'))
@@ -232,23 +211,23 @@ def delete(id):
 def issue():
     if request.method == 'POST':
         # Get data 
-        
         bname = request.form['bname']
         mname = request.form['mname']
         issuedate = request.form['issuedate']
         returndate = request.form['returndate']
         
         # Check if both the book and member exist
-        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes')
+        conn =  psycopg2.connect(**db_params)
         cursor = conn.cursor()
         
         # Check if the book exists
-        cursor.execute('SELECT * FROM Record.Book WHERE Title = ?', (bname,))
+        cursor.execute('SELECT * FROM Book WHERE Title=%s', (bname,))
         book = cursor.fetchone()
-        
+        print(book)
         # Check if the member exists
-        cursor.execute('SELECT * FROM Record.Member WHERE MemberName = ?', (mname,))
+        cursor.execute('SELECT * FROM Member WHERE MemberName=%s', (mname,))
         member = cursor.fetchone()
+        print(member)
         
         conn.close()
         
@@ -260,16 +239,16 @@ def issue():
                 new_quantity = book[5] - 1
                 
                 # Update the quantity of the book in the database
-                conn = pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes')
+                conn =  psycopg2.connect(**db_params)
                 cursor = conn.cursor()
-                cursor.execute('UPDATE Record.Book SET QuantityInStock = ? WHERE Title = ?', (new_quantity, bname))
+                cursor.execute('UPDATE Book SET QuantityInStock =%s WHERE Title =%s', (new_quantity, bname))
                 conn.commit()
-                conn.close()
+                #conn.close()
                 
                 # Insert the issue record into the database
-                data = (bname, mname, issuedate, returndate)
-                insert_dataissue(database, data)
                 
+                cursor.execute('INSERT INTO Issues(Title,MemberName,IssueDate,ReturnDate) VALUES (%s, %s, %s,%s)', (bname, mname, issuedate, returndate))
+                conn.commit()
                 return render_template('issues.html')
             else:
                 flash('Book is out of stock.', 'error')
@@ -283,9 +262,9 @@ def issue():
 
 @app.route('/displayissue')
 def displayissue():
-    with pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes') as conn:
+    with psycopg2.connect(**db_params) as conn:
         cursor=conn.cursor()
-        query=f'select * from Record.Issues'
+        query=f'select * from Issues'
         cursor.execute(query)
         data=cursor.fetchall()
     return render_template('displayissue.html',issues=data,info="Registered")
@@ -297,24 +276,23 @@ def returnbook():
         #Also take member_name from the frontend
         member_name=request.form['membername']
         # Retrieve book details from the database
-        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes')
+        conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM Record.Book WHERE Title= ? ', (book_name))
+        cursor.execute('SELECT * FROM Book WHERE Title= %s', (book_name))
         book = cursor.fetchone()
-       
-        
         if book:
             # Increment the quantity by 1
             new_quantity = book[5] + 1  # Assuming quantity is at index 5
             # Update the quantity in the database
-            conn = pyodbc.connect('DRIVER={SQL Server};SERVER=BLT210003;DATABASE=PROJECT;Trusted_Connection=yes')
+            conn = psycopg2.connect(**db_params)
             cursor = conn.cursor()
-            cursor.execute('UPDATE Record.Book SET QuantityInStock = ? WHERE Title = ?', (new_quantity, book_name))
+            cursor.execute('UPDATE Book SET QuantityInStock = %s WHERE Title = %s', (new_quantity, book_name))
+            
 
             # Delete book details from Records.Issues with particular title and member name
 
-            cursor.execute('DELETE FROM Record.Issues WHERE Title = ? AND MemberName=?', (book_name,member_name))
-            #cursor.execute('DELETE FROM Record.Issues WHERE Title = ? AND MemberName', (book_name,m_name))
+            cursor.execute('DELETE FROM Issues WHERE Title = %s AND MemberName= %s', (book_name,member_name))
+            
             conn.commit()
             
             conn.close()
@@ -326,11 +304,26 @@ def returnbook():
             #return redirect(url_for('display'))
     return render_template('return.html')
 
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
 
-    
-if __name__=='__main__':  
-    app.run(debug=False,host='0.0.0.0')
 
+
+
+
+
+
+        
+
+
+      
+   
+ 
+
+
+
+
+           
 
 
     
